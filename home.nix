f9e9362acbc12b4c77de7037d66b4bd153217c66
Devon Gardner <devon@goosur.com>
2025-12-13 22:53:49 -0500

    make symlink process modular thanks to tony

--- home.nix
+++ home.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    nvim = "nvim";
+  };
+in
+
 {
   imports = [
     ./modules/neovim.nix
@@ -9,13 +17,15 @@
   home.homeDirectory = "/home/devon";
   home.stateVersion = "25.11";
 
-  xdg.configFile."nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/devon/nixos-dotfiles/config/nvim";
+  # Map nixos-dotfiles/config to $XDG_HOME/config
+  xdg.configFile = builtins.mapAttrs (name: subpath: {
+    source = create_symlink "${dotfiles}/${subpath}";
     recursive = true;
-  };
+  }) configs;
 
+  # Configure programs
   programs.git = {
-  	enable = true;
+    enable = true;
     settings = {
       user = {
         name = "Devon Gardner";
