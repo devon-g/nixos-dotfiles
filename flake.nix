f894eeec6289ce840b151f9a0e6bb5959b1a4ef9
Devon Gardner <devon@goosur.com>
2025-12-19 17:23:07 -0500

    migrated to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,28 +1,20 @@
 {
-  description = "NixOS from Scratch";
+  description = "Laptop Experiment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        {
-          nixpkgs.overlays = [
-            (final: prev: {
-              unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-            })
-          ];
-        }
         ./configuration.nix
-        home-manager.nixosModules.default
+        home-manager.nixosModules.home-manager
         {
           home-manager = {
             useGlobalPkgs = true;
